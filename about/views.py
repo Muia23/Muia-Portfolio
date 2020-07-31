927597f66d8880a472844e9067377c960f09754e
Muia23 <wilfred.muema@gmail.com>
2020-07-31 16:55:13 +0300

    add the about me template, with the base layout

--- about/views.py
+++ about/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import datetime as dt
 # Create your views here.
+def home(request):
+    date = dt.date.today()
+    
+    
+    return render(request, 'home.html', {"date": date})
+
+def convert_dates(dates):
+    #Function that gets the weekday
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
+    #Return the actual day of the week
+    day = days[day_number]
+    return day

e2c8454d982bddd4055dac506a09087a20d09f47
Muia23 <wilfred.muema@gmail.com>
2023-02-19 23:17:53 +0300

    UX update Muia-Portfolio 2.0

--- static/js/home.js
+++ static/js/home.js
@@ -1,1 +1,20 @@
-console.log('hey');
+document.getElementById('prototypes').addEventListener('click', () => {
+    console.log("Prototype");
+    if (document.getElementById('proje').style.display == 'block'){
+        document.getElementById('proje').style.display = 'none';
+        document.getElementById('proto').style.display = 'flex';
+        !document.getElementById('prototypes').classList.contains('active')? document.getElementById('prototypes').classList.add('active'): document.getElementById('prototypes').classList.remove('active');        
+        !document.getElementById('projects').classList.contains('active')? document.getElementById('projects').classList.add('active'): document.getElementById('projects').classList.remove('active');        
+    }
+});
+
+document.getElementById('projects').addEventListener('click', () => {
+    console.log("Projects");
+    let proj_tog = document.getElementById('projects');
+    if (document.getElementById('proto').style.display == 'flex'){
+        document.getElementById('proto').style.display = 'none';
+        document.getElementById('proje').style.display = 'block';
+        !document.getElementById('projects').classList.contains('active')? document.getElementById('projects').classList.add('active'): document.getElementById('projects').classList.remove('active');        
+        !document.getElementById('prototypes').classList.contains('active')? document.getElementById('prototypes').classList.add('active'): document.getElementById('prototypes').classList.remove('active');
+    }
+});

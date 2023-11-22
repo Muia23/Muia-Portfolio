b084194f2cf80f52d5639b0ca6a9b33f6edd530f
Muia23 <wilfred.muema@gmail.com>
2023-11-22 05:34:54 +0300

    Muia-Portfolio2.0

--- about/urls.py
+++ about/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
     re_path(r'^$',views.home,name = 'home'),
-    re_path(r'^projects/$',views.projects,name = 'projects'),
+    re_path(r'^projects$',views.projects,name = 'projects'),
     re_path(r'^search/', views.search_results, name= 'search_results'),
     re_path(r'^projects/details/(\d+)/$',views.project_detail,name='projectsdetail'),
     re_path(r'^projects/collaborations$',views.project_collab,name='projectcollabs'),
